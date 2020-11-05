ee24f7b6f3c4b67f328a18ccf8e6560b38cba731
Chung Hong <conconart.info@gmail.com>
2020-11-05 14:56:23 -0700

    Add Game class to game.js

---
+++ game.js
@@ -0,0 +1,22 @@
+class Game {
+  constructor(player1, player2, board) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = board;
+  }
+  checkTurn(){
+
+  }
+  checkForWin(){
+
+  }
+  checkForDraw(){
+
+  }
+  saveWinsToStorage(){
+    //add to correct player's wins array
+  }
+  resetGame(){
+
+  }
+}
