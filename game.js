6e7ff33da3853d3467ecbb05da4fc76aedbfb7e1
Chung Hong <conconart.info@gmail.com>
2020-11-06 14:32:18 -0700

    Update board property in Game class

--- game.js
+++ game.js
@@ -2,16 +2,17 @@ class Game {
   constructor() {
     this.playerOne = new Player("Jim")
     this.playerTwo = new Player("Dwight")
-    this.board = {
-      boxOne: "",
-      boxTwo: "",
-      boxThree: "",
-      boxFour: "",
-      boxFive: "",
-      boxSix: "",
-      boxSeven: "",
-      boxEight: "",
-      boxNine: "",
+    this.board = [
+      {boxOne: ""},
+      {boxTwo: ""},
+      {boxThree: ""},
+      {boxFour: ""},
+      {boxFive: ""},
+      {boxSix: ""},
+      {boxSeven: ""},
+      {boxEight: ""},
+      {boxNine: ""},
+      ]
     }
   }
   checkTurn(){
@@ -20,8 +21,19 @@ class Game {
     //if player1 turn = true; player2 turn = false;
   }
   checkForWin(){
+    //winning conditions:
+    //comboOne: 1 + 2 + 3,
+    //comboTwo: 1 + 5 + 9,
+    //comboThree: 1 + 4 + 7,
+    //comboFour: 2 + 5 + 8,
+    //comboFive: 3 + 6 + 9,
+    //comboSix: 3 + 5 + 7,
+    //comboSeven: 4 + 5 + 6,
+    //comboEight: 7 + 8 + 9
+
     //if (check conditions) { player1.wins.push(this.board)}
     //else if(check conditions) {player2.wins.push(this.board)}
+    //if win, reset board
   }
   checkForDraw(){
     //if (check conditions) {claim draw, reset board}
