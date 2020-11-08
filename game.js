b58a6a4bede920763c01cd7e103424ec838fb723
Chung Hong <conconart.info@gmail.com>
2020-11-07 17:19:31 -0700

    Refactor names of game spaces

--- game.js
+++ game.js
@@ -19,14 +19,14 @@ class Game {
   };
   checkForWin(player){
     var winningCombos = [
-      [this.board.sqOne, this.board.sqTwo, this.board.sqThree],
-      [this.board.sqOne, this.board.sqFive, this.board.sqNine],
-      [this.board.sqOne, this.board.sqFour, this.board.sqSeven],
-      [this.board.sqTwo, this.board.sqFive, this.board.sqEight],
-      [this.board.sqThree, this.board.sqSix, this.board.sqNine],
-      [this.board.sqThree, this.board.sqFive, this.board.sqSeven],
-      [this.board.sqFour, this.board.sqFive, this.board.sqSix],
-      [this.board.sqSeven, this.board.sqEight, this.board.sqNine]
+      [this.board.one, this.board.two, this.board.three],
+      [this.board.one, this.board.five, this.board.nine],
+      [this.board.one, this.board.four, this.board.seven],
+      [this.board.two, this.board.five, this.board.eight],
+      [this.board.three, this.board.six, this.board.nine],
+      [this.board.three, this.board.five, this.board.seven],
+      [this.board.four, this.board.five, this.board.six],
+      [this.board.seven, this.board.eight, this.board.nine]
     ]
     for (var i = 0; i < winningCombos.length; i++) {
       if(winningCombos[i] === [player.token, player.token, player.token]) {
@@ -41,20 +41,23 @@ class Game {
     //if (check conditions) {claim draw, reset board}
    //if this.board is full with no winning combos, claim draw
   }
+  celebrateWin() {
+    //array of gifs to display upon winning
+  }
   saveWinsToStorage(){
     //add to correct player's wins array
   }
   resetGame(){
     this.board = [
-      {sqOne: ""},
-      {sqTwo: ""},
-      {sqThree: ""},
-      {sqFour: ""},
-      {sqFive: ""},
-      {sqSix: ""},
-      {sqSeven: ""},
-      {sqEight: ""},
-      {sqNine: ""},
+      {one: ""},
+      {two: ""},
+      {three: ""},
+      {four: ""},
+      {five: ""},
+      {six: ""},
+      {seven: ""},
+      {eight: ""},
+      {nine: ""},
     ]
   }
 }
