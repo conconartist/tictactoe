9db0f08b727e77cd3177fec61a9f037b5af53c75
Chung Hong <conconart.info@gmail.com>
2020-11-08 15:13:32 -0700

    Add query selectors and displayWinCount function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var gameText = document.querySelector(".game-text");
 var playerOneText = document.querySelector(".player-one");
 var playerTwoText = document.querySelector(".player-two");
+var playerOneWinCount = document.querySelector(".player-one-wins-counter");
+var playerTwoWincount = document.querySelector(".player-two-wins-counter")
 var one = document.querySelector("#one");
 var two = document.querySelector("#two");
 var three = document.querySelector("#three");
@@ -74,16 +76,25 @@ function determineNext(player){
 }
 
 function determineWin(player) {
-    updateWins()
+    updateWins(player)
     gameBoard.removeEventListener("click", playToken);
     displayWinText(player)
     //celebrateWin
     //reset game
+    displayWinCount(player)
 }
 
 function displayWinText(player) {
   gameText.classList.add("end")
   gameText.innerHTML = `${player.name} WINS!`
+}
+
+function displayWinCount(player) {
+  if(player === currentGame.playerOne) {
+  playerOneWinCount.innerText = `${player.wins.length}`
+  } else {
+  playerTwoWincount.innerText = `${player.wins.length}`
+  }
 }
 
 function determineDraw() {
@@ -114,7 +125,8 @@ function startNewGame() {
 //pull from local storage to keep wins display on refresh
 }
 
-function updateWins() {
+function updateWins(player) {
+  currentGame.saveWinsToStorage(player)
   //update text under Player's wins count
   //save number to storage, update player instance
 }
