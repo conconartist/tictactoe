128a3fff87af25a350c95e3ce70739469356e522
Chung Hong <conconart.info@gmail.com>
2020-11-09 15:49:57 -0700

    Add celebrationGifs array and celebrateWin function

--- game.js
+++ game.js
@@ -2,7 +2,24 @@ class Game {
   constructor() {
     this.playerOne = new Player("Jim", "./assets/IMG_8984.PNG");
     this.playerTwo = new Player("Dwight", "./assets/IMG_8985.PNG");
-    this.board = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    this.board = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    this.celebrationGifs = [
+      "https://media.giphy.com/media/KYElw07kzDspaBOwf9/giphy.gif",
+      "https://media.giphy.com/media/lMVNl6XxTvXgs/giphy.gif",
+      "https://media.giphy.com/media/nGzeO4uSxRUcg/giphy.gif",
+      "https://media.giphy.com/media/QvXe5wq8w94A0/giphy.gif",
+      "https://media.giphy.com/media/CM8sq5yoIqzTO/giphy.gif",
+      "https://media.giphy.com/media/1yJEEsgy4q2bu/giphy.gif",
+      "https://media.giphy.com/media/6wmz6Qo40eTDf4tW3Z/giphy.gif",
+      "https://media.giphy.com/media/JebM0Qb1bAouI/giphy.gif",
+      "https://media.giphy.com/media/2hoC9UkqBb60M/giphy.gif",
+      "https://media.giphy.com/media/AatW9hUhdcX5e/giphy.gif",
+      "https://media.giphy.com/media/12yZ3KEf43DfLG/giphy.gif",
+      "https://media.giphy.com/media/CTYlZFhOV6qvS/giphy.gif",
+      "https://media.giphy.com/media/yb9IgtTZGAWgo/giphy.gif",
+      "https://media.giphy.com/media/JebM0Qb1bAouI/giphy.gif",
+      "https://media.giphy.com/media/czoS1CAP2YZBS/giphy.gif"
+    ]
   };
   togglePlayerTurn(){
     if (this.playerOne.turn === true) {
@@ -40,8 +57,9 @@ class Game {
     }
     return true;
   }
-  celebrateWin(player) {
-    //array of gifs to display upon winning
+  celebrateWin() {
+    var randomIndex = Math.floor(Math.random() * this.celebrationGifs.length);
+    return this.celebrationGifs[randomIndex]
   }
   saveWinsToStorage(player){
     player.wins.push(this.board)
