27dcbbd578e616514d7969c5449956db4473e301
Chung Hong <conconart.info@gmail.com>
2020-11-08 17:46:14 -0700

    Add functionality to saveWinsToStorage function in Player class

--- player.js
+++ player.js
@@ -1,13 +1,15 @@
 class Player {
   constructor(name, imgSrc) {
-    this.id = Date.now();
+    this.id = Date.now();//can i delete?
     this.name = name;
     this.token = imgSrc;
     this.wins = [];
     this.turn = false;
   }
   saveWinsToStorage() {
-    //save array of this.wins to local storage
+    var winsToStore = this.wins;
+    var stringifiedWins = JSON.stringify(winsToStore);
+    localStorage.setItem('storedWins', stringifiedWins)
   }
   retrieveWinsFromStorage() {
     //
